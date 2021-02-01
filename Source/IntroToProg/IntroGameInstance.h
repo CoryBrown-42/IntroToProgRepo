d22308658ce454a96b0a6df62aa5f6928229d497
CoryBrown-42 <yroc.999@gmail.com>
2021-02-01 13:07:28 -0500

    Replication overview - Game Instance

---
+++ Source/IntroToProg/IntroGameInstance.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "IntroGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTROTOPROG_API UIntroGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UIntroGameInstance(const FObjectInitializer & ObjectInitializer);
+
+	virtual void Init();
+
+	UFUNCTION(BlueprintCallable)
+	void SayHi();
+
+	UFUNCTION(exec)
+	void Kill();
+	
+};
