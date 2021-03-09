6b3bc4a1bec4607bd4221afea2d5d64f595aa46d
CoryBrown-42 <yroc.999@gmail.com>
2021-03-09 13:46:06 -0500

    blockout start

--- Source/IntroToProg/IntroToProgGameMode.cpp
+++ Source/IntroToProg/IntroToProgGameMode.cpp
@@ -7,7 +7,7 @@
 AIntroToProgGameMode::AIntroToProgGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprint/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
